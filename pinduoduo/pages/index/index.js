b7c9e9c00db988695ba6566153b6a9f7c9df3879
LestaSan <295510545@qq.com>
2018-06-03 23:08:08 +0800

    将部分图片路径改成网络链接

--- pinduoduo/pages/index/index.js
+++ pinduoduo/pages/index/index.js
@@ -69,13 +69,13 @@ Page({
       }
     ],
     headerpic: [
-      {src:'../../images/headerPic1.png'},
-      {src:'../../images/headerPic2.png'},
-      {src:'../../images/headerPic3.png'},
-      {src:'../../images/headerPic4.png'},
-      {src:'../../images/headerPic5.png'},
-      {src:'../../images/headerPic6.png'},
-      {src:'../../images/headerPic7.png'}
+      {src:'//t00img.yangkeduo.com/t04img/images/2018-06-03/a2a9c4f596faa47e2a387751f4aa8e39.jpeg?imageMogr2/format/webp/quality/50'},
+      {src:'//t00img.yangkeduo.com/t09img/images/2018-06-01/d48e3e2294cea2ddf7f52211d218657f.jpeg?imageMogr2/format/webp/quality/50'},
+      {src:'//t00img.yangkeduo.com/t05img/images/2018-06-03/a4a01001373b85b521b1db9e64ff32ee.jpeg?imageMogr2/format/webp/quality/50'},
+      {src:'//t00img.yangkeduo.com/t01img/images/2018-06-03/3728f696f66f4a6da1bee861afdf6e73.jpeg?imageMogr2/format/webp/quality/50'},
+      {src:'//t00img.yangkeduo.com/t03img/images/2018-06-03/8009baf6f2a86704fb9d137af8bad103.jpeg?imageMogr2/format/webp/quality/50'},
+      {src:'//t00img.yangkeduo.com/t05img/images/2018-06-03/af0e581e74acb90343c162a08b0cd1b4.jpeg?imageMogr2/format/webp/quality/50'},
+      {src:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4119613175,2906257134&fm=27&gp=0.jpg'}
     ],
     blkpic1: [
       {
